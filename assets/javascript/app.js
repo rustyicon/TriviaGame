cdaec817431c7a77296ab7e82d23e637830ef60a
Sharon Zavala <sharon@MacBook-Pro.local>
2017-04-20 07:40:59 -0700

    updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,70 @@
-var questions = []
+$(document).ready(function() {
+
+var display;
+var questions = [
+				{
+					question:'Ponyo is the story of a little fish that catches a glimpse of the human world and wants to become part of it. What story is Ponyo losely based on:'
+					answer1:'Finding Nemo'
+					answer2:'Big Fish'	
+					answer3:'Jaws'
+					answer4:'The Little Mermaid'
+				}
+				{
+					question:'With a sick ailing mother, and a busy university professor father, Totoro is the story of Satsuki and Mei, whom are left  :'
+					answer1:''
+					answer2:''	
+					answer3:''
+					answer4:''
+				}
+				{
+					question:':'
+					answer1:''
+					answer2:''	
+					answer3:''
+					answer4:''
+				}
+				{
+					question:':'
+					answer1:''
+					answer2:''	
+					answer3:''
+					answer4:''
+				}
+				{
+					question:':'
+					answer1:''
+					answer2:''	
+					answer3:''
+					answer4:''
+				}
+				{
+					question:':'
+					answer1:''
+					answer2:''	
+					answer3:''
+					answer4:''
+				}
+
+				];
 var answers = []
+
 
 $("#button").click(startGame)
 
+$('#done').click(endGame)
+
 setTimeout(timeUp, 120000)
+
+
+
+	
+
 
 function startGame({
 
 })
+
+
+function endGame (){
+
+}
